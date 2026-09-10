14e71234c4fecce9c91ad2bb55dfc5c0ab9a8d86
Tanqurai <nuadmissionnu2023@gmail.com>
2026-09-11 02:05:46 +0500

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import { Accordion } from "./components/Accordion";
 import { Topic } from "./components/Topic";
 import roadmapData from './texts/roadmap.json';
+import { Head } from "./components/Head";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[#1E1E1E] text-white p-6 font-sans">
-      <main className="max-w-3xl mx-auto space-y-4">
-        {roadmapData.levels.map((level) => (
-          <Accordion
-            key={level.id}
-            level={level.level}
-            description={level.description}
-            topics={level.topics.map((topic) => (
-              <Topic
-                key={topic.id}
-                title={topic.title}
-                description={topic.description}
-                urls={topic.urls}
-              />
-            ))}
-          />
-        ))}
-      </main>
+    <div className="bg-[#1E1E1E]">
+      <div className="sticky top-0 z-50 bg-[#1E1E1E]/50 backdrop-blur-md text-white p-4 border-b border-[#251E38]/50">
+        <Head />
+      </div>
+      <div className="min-h-screen bg-[#1E1E1E] text-white p-6 flex flex-col items-center justify-center">
+        <main className="max-w-3xl mx-auto space-y-4">
+          {roadmapData.levels.map((level) => (
+            <Accordion
+              key={level.id}
+              level={level.level}
+              description={level.description}
+              topics={level.topics.map((topic) => (
+                <Topic
+                  key={topic.id}
+                  title={topic.title}
+                  description={topic.description}
+                  urls={topic.urls}
+                />
+              ))}
+            />
+          ))}
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Head.tsx
@@ -0,0 +1,16 @@
+import datasciImage from "../images/datasci.jpg";
+
+export function Head() {
+  return (
+    <div className="flex items-center space-x-4">
+
+      <div className="p-1 rounded-2xl border border-[#8E52FF]/30 bg-[#251E38]/50 shadow-[0_0_15px_rgba(142,82,255,0.15)] flex items-center justify-center">
+        <img src={datasciImage} alt="datasci logo" className="w-10 h-10 rounded-xl object-cover"/>
+      </div>
+
+      <h1 className="text-3xl font-extrabold text-white tracking-tight">
+        DataSci 101 Roadmap
+      </h1>
+    </div>
+  );
+}
